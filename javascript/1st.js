f6ec63994ccbef89403f6ea2a7957fb5eda868a4
naveen6010 <naveen972821@gmail.com>
2025-05-01 19:28:00 +0530

    push all file

---
+++ javascript/1st.js
@@ -0,0 +1,43 @@
+// variables
+// Name ="nishu";
+// age=2;
+// x = null;
+// y= undefined
+// hello= true;
+// console.log(hello);
+// console.log(Name);
+
+// let var const
+// const z= 2
+// console.log(z);
+// const STUDENT = {
+// Name :"nishu",
+// age: 24,
+// salary: 1000000000000,
+// };
+// STUDENT["age"]= STUDENT["age"] + 1;
+// console.log(STUDENT.age)
+// const product = {
+//     title: "ballpen",
+//      name : "parker jotter standard ct ball pen",
+//      rating : 3,
+//      isdeal : true,
+//      offer : 5 , 
+//      price : 270
+// }
+// console.log (product);
+// const profile={
+// title : "instagram",
+// username : "shrdakhapra",
+// isfollow : false,
+// followers : 569,
+// following : 4,
+// }
+// console.log (profile);
+
+
+
+
+
+
+
---
+++ javascript/2nd.js
@@ -0,0 +1,107 @@
+// arithmetics operators
+// let a = 4;
+// let b = 3;
+// console.log("a=",a,"b=", b);
+// console.log("a - b =",  a-b);
+// console.log("a + b =",  a+b);
+// console.log("a * b =",  a*b);
+// console.log("a / b =",  a/b);
+// console.log("a % b =",  a%b);
+//console.log("a ** b =",  a**b);
+/* modulus reminder
+exponentition power*/
+/*
+unary operator
+increment
+decrement
+++a
+a++
+*/
+// console.log("a=",a,"b=", b);
+// a--;
+// console.log("++a=",a++,);
+ 
+// assignment opertors
+/* 
+=, +=,-=,*=,**=,%=
+*/
+// a -= 2;
+// console.log("a=",a,"b=", b);
+
+// comparison operators
+/* ==
+===
+!=
+!==
+>
+>=
+<
+<=*/
+// console.log("a==b", a<=b);
+
+// LOGICAL OPERATOR 
+/*
+and &&
+or||
+not!
+*/
+// let a = 4;
+// let b = 3;
+// let cond1 = a>b;
+// let cond2 = a<b;
+// console.log("cond1 && cond2 =", !(cond1));
+
+// conditional statement
+
+// let age= 199;
+// let mode = 'dark';
+// let color;
+// if ( age=== 18) {
+//     console.log ("you can buy a mobile phone")
+// } else if (age===20){
+//     console.log('absolutely you can buy a phone')
+// }else {
+//     console.log('you cannot buy a phone')
+// }
+
+// if( mode === "dark") {
+//     color = 'black';
+// } else {
+//     color = 'white';
+// }
+// console.log(color)
+// let num= 10 ;
+// if(num%2 === 0) {
+//     console.log(num," is even")
+// } else {
+//     console.log(num," is odd")
+// }
+// let age= 18;
+
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result)
+
+// let num = prompt ("Enter a number");
+// if (num % 3 === 0){
+// console.log(num," is multiple of 3")
+// } else { console.log(num," is not multiple of 3")}
+
+let score = prompt('enter your score')
+if (score>=80 && score <= 100){
+    Grade = 'A';
+} else if (score>=70 && score <= 89) {
+    Grade = 'B';
+} if (score>=60 && score <= 69) {
+Grade = 'C';
+} else if (score>=50 && score <= 59) {
+Grade = 'D';
+} if (score>=0 && score <= 49) {
+   Grade = 'F'
+};
+console.log("your grades are",Grade );
+naveen={
+    naveen 
+}
+
+
+
